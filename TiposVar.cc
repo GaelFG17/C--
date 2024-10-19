a05e6b334cf02f2d4a33c9eeb961390f61aba695
GaelFG17 <gaelfg1720@gmail.com>
2024-10-18 22:25:41 -0600

    Variables en c++

---
+++ TiposVar.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    // Declaración de variables
+    int entero = 10;
+    float flotante = 5.5;
+    double doble = 3.14159;
+    char caracter = 'A';
+    bool booleano = true;
+
+    // Imprimir variables
+    std::cout << "Entero: " << entero << std::endl;
+    std::cout << "Flotante: " << flotante << std::endl;
+    std::cout << "Doble: " << doble << std::endl;
+    std::cout << "Caracter: " << caracter << std::endl;
+    std::cout << "Booleano: " << booleano << std::endl;
+
+    return 0;
+}
