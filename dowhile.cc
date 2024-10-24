f70b9adcbb959f3ee26832ed570e75819f006516
GaelFG17 <gaelfg1720@gmail.com>
2024-10-23 22:46:12 -0600

    do while

---
+++ dowhile.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int i = 0;
+
+    do {
+        std::cout << "El valor de i es: " << i << std::endl;
+        i++;
+    } while (i < 5);
+
+    return 0;
+}
